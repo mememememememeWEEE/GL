f2985b9e91c642e672d82d0ea0f1c6d8f8178e7f
mememememememeWEEE <randomhumanbeing12345@gmail.com>
2023-01-10 09:16:27 -0800

    added glew

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-gcc main.cpp lib/glfw/libglfw3.a -lopengl32 -lgdi32 -o main -O2 -Wall -Wextra
+gcc main.cpp lib/glew/libglew32.a lib/glfw/libglfw3.a -lopengl32 -lgdi32 -o main -O2 -Wall -Wextra
--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 #define GL_SILENCE_DEPRECATION
+#define GLEW_STATIC
 
+#include <cstdio>
+#include "lib/glew/glew.h"
 #include "lib/glfw/glfw3.h"
+
+void error_callback([[maybe_unused]] int error, const char* description) {
+	fprintf(stderr, "%s", description);
+}
 
 int main(void) {
 	
 	GLFWwindow* window;
 	if (!glfwInit()) return -1;
 
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwSetErrorCallback(error_callback);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	window = glfwCreateWindow(600, 400, "Hello", NULL, NULL);
 	if (!window) {glfwTerminate(); return -1;}
 
 	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) return -1;
 
 	while (!glfwWindowShouldClose(window)) {
 		//render here:
