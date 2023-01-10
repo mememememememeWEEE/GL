e87d5e1e556435b92585f565f35e710f14d49505
mememememememeWEEE <randomhumanbeing12345@gmail.com>
2023-01-10 10:35:10 -0800

    Made Red Triangle

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-gcc main.cpp lib/glew/libglew32.a lib/glfw/libglfw3.a -lopengl32 -lgdi32 -o main -O2 -Wall -Wextra
+gcc main.cpp lib/glew/libglew32.a lib/glfw/libglfw3.a -lopengl32 -lgdi32 -o main -std=c++23 -O2 -Wall -Wextra -DDEBUG
--- main.cpp
+++ main.cpp
@@ -2,8 +2,47 @@
 #define GLEW_STATIC
 
 #include <cstdio>
+#include <cstdlib>
 #include "lib/glew/glew.h"
 #include "lib/glfw/glfw3.h"
+
+static unsigned int CompileShader(unsigned int type, const char* source) {
+	unsigned int id = glCreateShader(type);
+	glShaderSource(id, 1, &source, nullptr);
+	glCompileShader(id);
+
+#ifdef DEBUG
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*) __builtin_alloca(length * sizeof(char)); //based stack allocation versus virgin malloc
+		glGetShaderInfoLog(id, length, &length, message);
+		printf("Failed to compile %s shader: \n%s\n", (type == GL_VERTEX_SHADER ? "vertex" : "fragment"), message);
+		glDeleteShader(id);
+		exit(-1);
+	}
+#endif
+	
+	return id;
+}
+
+static int CreateShader(const char* vertexShader, const char* fragmentShader) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
 
 void error_callback([[maybe_unused]] int error, const char* description) {
 	fprintf(stderr, "%s", description);
@@ -12,7 +51,12 @@ void error_callback([[maybe_unused]] int error, const char* description) {
 int main(void) {
 	
 	GLFWwindow* window;
-	if (!glfwInit()) return -1;
+	if (!glfwInit()) {
+#ifdef DEBUG
+		printf("GLFW initialization failed\n");
+#endif
+		return -1;
+	}
 
 	glfwSetErrorCallback(error_callback);
 
@@ -22,21 +66,63 @@ int main(void) {
 	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	window = glfwCreateWindow(600, 400, "Hello", NULL, NULL);
-	if (!window) {glfwTerminate(); return -1;}
+	if (!window) {
+#ifdef DEBUG
+		printf("Window creation failed\n");
+#endif
+		glfwTerminate();
+		return -1;
+	}
 
 	glfwMakeContextCurrent(window);
 
-	if (glewInit() != GLEW_OK) return -1;
+	if (glewInit() != GLEW_OK) {
+#ifdef DEBUG
+		printf("GLEW initialization failed\n");
+#endif
+		return -1;
+	}
+
+#ifdef DEBUG
+	printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
+#endif
+
+	float positions[6] = {
+		-0.5f, -0.5f,
+		0.0f, 0.5f, 
+		0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); 
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
+
+	const char * vertexShader = 
+		"#version 330 core\n"
+		"layout(location=0) in vec4 position;\n"
+		"void main() {\n"
+		"gl_Position = position;\n"
+		"}\n";
+
+	const char * fragmentShader = 
+		"#version 330 core\n"
+		"layout(location=0) out vec4 color;\n"
+		"void main() {\n"
+		"color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"}\n";
+
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
 
 	while (!glfwWindowShouldClose(window)) {
 		//render here:
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//exchange front and back buffers
 		glfwSwapBuffers(window);
